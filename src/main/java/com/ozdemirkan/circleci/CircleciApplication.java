8d2c39481c39ccc4772aaa5cb48961f33c453c43
Melih Ozdemirkan <ozdemirkan@gmail.com>
2019-06-26 10:11:58 +0100

    init

---
+++ src/main/java/com/ozdemirkan/circleci/CircleciApplication.java
@@ -0,0 +1,13 @@
+package com.ozdemirkan.circleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleciApplication.class, args);
+	}
+
+}
